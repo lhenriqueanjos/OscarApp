8572d8441621f3ce656c3ef20557c5d25fd43a8f
Luiz Henrique Nascimento dos Anjos <lhenriqueanjos@gmail.com>
2017-06-19 20:57:33 -0300

    adiciona AsyncTask para o login

--- app/src/main/java/tads/ufpr/br/oscarapp/MainActivity.java
+++ app/src/main/java/tads/ufpr/br/oscarapp/MainActivity.java
@@ -1,30 +1,56 @@
 package tads.ufpr.br.oscarapp;
 
-import android.app.Activity;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import tads.ufpr.br.oscarapp.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MAIN_ACTIVITY";
 
+    private EditText emailTxt;
+    private EditText passwordTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailTxt = (EditText) findViewById(R.id.emailTxt);
+        passwordTxt = (EditText) findViewById(R.id.passwordTxt);
     }
 
     public void onClickLogin(View view) {
-        Toast t = Toast.makeText(this, "Login", Toast.LENGTH_SHORT);
-        t.show();
         Log.d(TAG, "Login");
+
+        Map<String, String> loginMap = new HashMap<>();
+        loginMap.put("email", emailTxt.getText().toString());
+        loginMap.put("password", passwordTxt.getText().toString());
+
+        LoginTask task = new LoginTask();
+        task.execute(loginMap);
+    }
+
+    private class LoginTask extends AsyncTask<Map<String, String>, Void, User> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected User doInBackground(Map<String, String>... maps) {
+            Log.d(TAG, maps[0].keySet().toString());
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/tads/ufpr/br/oscarapp/model/User.java
@@ -0,0 +1,49 @@
+package tads.ufpr.br.oscarapp.model;
+
+public class User {
+    private Long id;
+    private String userName;
+    private String password;
+    private Long movieId;
+    private Long directorId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getDirectorId() {
+        return directorId;
+    }
+
+    public void setDirectorId(Long directorId) {
+        this.directorId = directorId;
+    }
+}
