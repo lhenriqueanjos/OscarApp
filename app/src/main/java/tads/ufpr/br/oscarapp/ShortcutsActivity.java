e36fe0378d54d3e731796ae870d4772369f7bc8f
Luiz Henrique Nascimento dos Anjos <lhenriqueanjos@gmail.com>
2017-06-21 20:47:55 -0300

    adiciona tratamento para as opções de menu

--- app/src/main/java/tads/ufpr/br/oscarapp/ShortcutsActivity.java
+++ app/src/main/java/tads/ufpr/br/oscarapp/ShortcutsActivity.java
@@ -2,9 +2,13 @@ package tads.ufpr.br.oscarapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class ShortcutsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SHORTCUTS_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,25 @@ public class ShortcutsActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.listMoviesItem:
+                Log.d(TAG, "listar filmes");
+                return true;
+            case R.id.listDirectorsItem:
+                Log.d(TAG, "listar diretores");
+                return true;
+            case R.id.confirmItem:
+                Log.d(TAG, "confirmar voto");
+                return true;
+            case R.id.exiItem:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
