c9faf011567973ee050df3bda81ac867da0e69bb
Luiz Henrique Nascimento dos Anjos <lhenriqueanjos@gmail.com>
2017-06-25 23:56:59 -0300

    limpeza de imports na MainActivity

--- app/src/main/java/tads/ufpr/br/oscarapp/MainActivity.java
+++ app/src/main/java/tads/ufpr/br/oscarapp/MainActivity.java
@@ -1,43 +1,28 @@
 package tads.ufpr.br.oscarapp;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.IllegalFormatCodePointException;
 import java.util.Map;
 
-import tads.ufpr.br.oscarapp.model.Director;
-import tads.ufpr.br.oscarapp.model.Movie;
 import tads.ufpr.br.oscarapp.model.User;
 
 public class MainActivity extends AppCompatActivity {
